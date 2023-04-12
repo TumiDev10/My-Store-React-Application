a0b7c5d60819f014302c4ff5e88f2a334ed0dd5c
Tumi Mashigo <tumi.mashigo15@gmail.com>
2023-04-12 14:21:44 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './About.css';
-import './aboutme.jpg';
+import aboutImage from './aboutme.jpg';
 
 function AboutPage() {
+  // const teamMembers = [
+  //   { name: "Tumi Mashigo", position: "CEO" },
+  //   { name: "Tshepo Ngobeni", position: "COO" },
+  //   { name: "Bob Johnson", position: "CTO" },
+  //   { name: "Lisa Anderson", position: "CFO" },
+  // ];
+
   return (
     <div className="about-page">
       <header>
         <h1>About Us - My Store</h1>
       </header>
       <div className="container">
-      <div className="image">
-        <img src="https://placekitten.com/200/200" alt="profile" />
+        <div className="image">
+          <img src={aboutImage} alt="profile" className="profile-image" />
         </div>
         <div className="content">
           <h2>Our Story</h2>
           <p>
             My Store was founded in 2023 with a mission to provide the best products and services to our customers. Over the years, we have grown into a leading provider of widgets, gadgets, and gizmos, serving customers all over the world.
           </p>
-          <h2>Our Team</h2>
-          <p>
-            Our team is made up of passionate professionals who are dedicated to providing exceptional customer service and delivering high-quality products. We believe in working hard, having fun, and treating each other with respect.
-          </p>
+          <div className="cta">
+            <Link to="/products">
+              <button className="cta-button">Explore Our Products</button>
+            </Link>
+          </div>
+          </div>
+         
         </div>
-       
       </div>
-    </div>
+    // </div>
   );
 }
 
