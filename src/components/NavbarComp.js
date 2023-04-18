ffe8d83a7799392d7444203f093fcb7c1570b202
Tumi Mashigo <tumi.mashigo15@gmail.com>
2023-04-18 15:42:02 +0200

    new feature added

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,68 +1,106 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";    
-import Products from "./Products";
-import Cart from "./Cart";
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Products from './Products';
+import Cart from './Cart';
 import { Login, Signup } from './Login';
-import "./Footer.css";
-
+import './Footer.css';
 
 function BasicExample() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
   return (
-    
     <Router>
       <div>
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
-            <Navbar.Brand href='/'>My Store</Navbar.Brand>
+            <Navbar.Brand href="/">My Store</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link as={Link} to="/Home">Home</Nav.Link>
-                <Nav.Link as={Link} to="/about">About</Nav.Link>
-                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-                <Nav.Link as={Link} to="/products">Products</Nav.Link>
+                <Nav.Link as={Link} to="/home">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/about">
+                  About
+                </Nav.Link>
+                <Nav.Link as={Link} to="/contact">
+                  Contact
+                </Nav.Link>
+                <Nav.Link as={Link} to="/products">
+                  Products
+                </Nav.Link>
               </Nav>
-              <Nav>
-                <Nav.Link as={Link} to="/login">Logout</Nav.Link>
-              </Nav>
+              {loggedIn ? (
+                <Nav>
+                  <Nav.Link as={Link} to="/login" onClick={handleLogout}>
+                    Logout
+                  </Nav.Link>
+                </Nav>
+              ) : (
+                <Nav>
+                  <Nav.Link as={Link} to="/login">
+                    Login
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/signup">
+                    Signup
+                  </Nav.Link>
+                </Nav>
+              )}
             </Navbar.Collapse>
           </Container>
         </Navbar>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/products" element={<Products />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/home" element={<Home />} />  
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} /> 
-          <Route exact path="/" component={Products} />
-          <Route path="/cart" render={() => <Cart cart={JSON.parse(localStorage.getItem('cart')) || []} />} />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
-       <footer>
-        
-         <div className="footer-container-fluid">
-         <ul className="social-media-icons">
-         <li><a href="http://www.twitter.com/twizztumi"><i className="fab fa-twitter"></i></a></li>
-         <li><a href="http://www.facebook.com/tumitwizzmashigo"><i className="fab fa-facebook-f"></i></a></li>
-         <li><a href="http://www.instagram.com/tumi_mashigo"><i className="fab fa-instagram"></i></a></li>
-        </ul>
-        <div className='copyright'>
-        <p>&copy; 2023 My Store. All rights reserved.</p>
-        </div>
-    
-  </div>
-</footer>
-
+        <footer>
+          <div className="footer-container-fluid">
+            <ul className="social-media-icons">
+              <li>
+                <a href="http://www.twitter.com/twizztumi">
+                  <i className="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="http://www.facebook.com/tumitwizzmashigo">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li>
+                <a href="http://www.instagram.com/tumi_mashigo">
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </li>
+            </ul>
+            <div className="copyright">
+              <p>&copy; 2023 My Store. All rights reserved.</p>
+            </div>
+          </div>
+        </footer>
       </div>
     </Router>
   );
+
 }
 
 export default BasicExample;
